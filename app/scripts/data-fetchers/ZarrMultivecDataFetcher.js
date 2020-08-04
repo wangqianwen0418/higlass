ddf5700bc87ec9aa384352197212ebee5d2a3d75
mkeller <7525285+keller-mark@users.noreply.github.com>
2020-08-04 18:29:46 -0400

    WIP: abs2genomic

--- app/scripts/data-fetchers/ZarrMultivecDataFetcher.js
+++ app/scripts/data-fetchers/ZarrMultivecDataFetcher.js
@@ -1,5 +1,23 @@
 import slugid from 'slugid';
 import { HTTPStore, openArray } from 'zarr';
+import { absToChr } from '../utils';
+import { parseChromsizesRows } from '../ChromosomeInfo';
+
+/**
+ *
+ * @param {number[]} chromSizes An array of the lengths of the chromosomes [1000,...]
+ * @param {number} startPos The starting genomic position
+ * @param {number} endPos The ending genomic position
+ */
+function abs2genomic(chromSizes, absStartPos, absEndPos) {
+  const chromInfo = parseChromsizesRows(chromSizes);
+  const [chrStart, chrStartPos] = absToChr(absStartPos, chromInfo);
+  const [chrEnd, chrEndPos] = absToChr(absEndPos, chromInfo);
+  return [
+    { chr: chrStart, pos: chrStartPos },
+    { chr: chrEnd, pos: chrEndPos },
+  ];
+}
 
 class ZarrMultivecDataFetcher {
   constructor(dataConfig) {
@@ -29,23 +47,14 @@ class ZarrMultivecDataFetcher {
       })
       .then(attrs => {
         this.tilesetInfoLoading = false;
-        const retVal = attrs;
-        console.log(attrs);
 
-        /*
-            const TILE_SIZE = 1024;
-            let retVal = {};
-            // retVal[this.trackUid] = {
-            retVal = {
-            tile_size: TILE_SIZE,
-            max_zoom: Math.ceil(
-                Math.log(this.gbJson[0].size / TILE_SIZE) / Math.log(2)
-            ),
-            max_width: this.gbJson[0].size,
-            min_pos: [0],
-            max_pos: [this.gbJson[0].size]
-            };
-            */
+        const max_pos_0 = attrs.max_pos[0];
+        const tile_size = attrs.tile_size;
+        const retVal = {
+          ...attrs,
+          max_width: max_pos_0,
+          max_zoom: Math.ceil(Math.log(max_pos_0 / tile_size) / Math.log(2)),
+        };
 
         if (callback) {
           callback(retVal);
@@ -98,14 +107,59 @@ class ZarrMultivecDataFetcher {
   }
 
   tile(z, x) {
+    const { store } = this;
+    console.log(`tile z: ${z}, x: ${x}`);
     return this.tilesetInfo().then(tsInfo => {
+      // Adapted from clodius.tiles.multivec.get_single_tile
+      // Reference: https://github.com/higlass/clodius/blob/develop/clodius/tiles/multivec.py#L66
+
+      // z is the index of the resolution that should be selected.
+      // Resolution is size of each bin (except for the last bin in each chromosome).
+      const resolution = tsInfo.resolutions[z];
+      const tileSize = tsInfo.tile_size;
+
+      // Where in the data does the tile start and end?
+      const tileStart = x * tileSize * resolution;
+      const tileEnd = tileStart + tileSize * resolution;
+
+      // chromSizes is an array of "tuples" [ ["chr1", 1000], ... ]
+      const chromSizes = tsInfo.chromSizes;
+
+      // Adapted from clodius.tiles.multivec.get_tile
+      // Reference: https://github.com/higlass/clodius/blob/develop/clodius/tiles/multivec.py#L110
+
+      const binsize = resolution;
+      const arrays = [];
+      let count = 0;
+
+      // keep track of how much data has been returned in bins
+      let currentBinnedDataPosition = 0;
+      let currentDataPosition = 0;
+
+      let numAdded = 0;
+      let totalLength = 0;
+
+      const [genomicStart, genomicEnd] = abs2genomic(
+        chromSizes,
+        tileStart,
+        tileEnd,
+      );
+      const { chr: chrStart, pos: chrStartPos } = genomicStart;
+      const { chr: chrEnd, pos: chrEndPos } = genomicEnd;
+      console.log(binsize, genomicStart, genomicEnd);
+
       const tileWidth = +tsInfo.max_width / 2 ** +z;
+      console.log(tileWidth);
 
-      // get the bounds of the tile
-      const minX = tsInfo.min_pos[0] + x * tileWidth;
-      const maxX = tsInfo.min_pos[0] + (x + 1) * tileWidth;
+      /*
+        const tileWidth = +tsInfo.max_width / 2 ** +z;
 
-      const scaleFactor = 1024 / 2 ** (tsInfo.max_zoom - z);
+        // get the bounds of the tile
+        const minX = tsInfo.min_pos[0] + x * tileWidth;
+        const maxX = tsInfo.min_pos[0] + (x + 1) * tileWidth;
+
+        const scaleFactor = 1024 / 2 ** (tsInfo.max_zoom - z);
+        */
 
       return [];
     });
